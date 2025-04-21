e054a91cca65e33762f5f821bc3e8f5916462966
Boris <boriscampins@gmail.com>
2025-04-20 20:36:31 -0500

    serializadores

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,4 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settingS.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
